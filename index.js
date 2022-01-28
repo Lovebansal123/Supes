39b39328c7be36d5b00a3a607a80a5204497a1e3
Love Bansal <bansallove123@gmail.com>
2022-01-28 11:14:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+// loader 
+
+var preload = document.getElementById("loader_container");
+function myfunction() {
+    preload.style.top = "-100%";
+   
+
+}
+
+
+
+
+// navbar 
+const mainMenu = document.querySelector('.nav-left');
+const closeMenu = document.querySelector('.hamburger-close');
+const openMenu = document.querySelector('.hamburger-open');
+
+
+
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close() {
+    mainMenu.style.top = '-100%';
+}
+
+
+
+// slider 
+ var swiper = new Swiper(".myswiper2", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+ pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
+        },
+        loop: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+ 
+
+
+   
+
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+          },
+        // slidesPerGroup: 4,
+        freeMode: true,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 5,
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 15,
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+        },
+    });
+
+
